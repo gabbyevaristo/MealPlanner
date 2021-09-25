0d4e2d41d189a33430d34f3a36ec7b884b271202
gabbyevaristo <gabbyevaristo@Gabbys-Air.attlocal.net>
2021-09-25 02:25:37 -0700

    Revert "migrate to node server"
    This reverts commit e414f8d67c7f0bdac0586b92fd50ba2d7e1742e7.

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import usersRoutes from './routes/usersRoutes.js';
+import recipeRoutes from './routes/recipeRoutes.js';
+
+const app = express();
+const PORT = process.env.REACT_APP_PORT || 5000;
+
+// Initialize middleware
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
+app.use(express.json());
+
+// Add routes
+app.use('/users', usersRoutes);
+app.use('/recipe', recipeRoutes);
+
+mongoose.connect(
+    process.env.REACT_APP_MONGO_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log('CONNECTED TO DATABASE')
+);
+
+app.listen(PORT, () => console.log('SERVER RUNNING'));
